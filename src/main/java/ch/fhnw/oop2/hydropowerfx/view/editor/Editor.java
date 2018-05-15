0325f22e61591abdc42e33d6dc23521bef20089b
rogerkreienbuehl <r.kreienbuehl>
2018-05-15 06:02:31 +0200

    Added fields to test

--- src/main/java/ch/fhnw/oop2/hydropowerfx/view/editor/Editor.java
+++ src/main/java/ch/fhnw/oop2/hydropowerfx/view/editor/Editor.java
@@ -1,9 +1,11 @@
 package ch.fhnw.oop2.hydropowerfx.view.editor;
 
+import ch.fhnw.oop2.hydropowerfx.presentationmodel.PowerStation;
 import ch.fhnw.oop2.hydropowerfx.presentationmodel.RootPM;
 import ch.fhnw.oop2.hydropowerfx.view.RootPanel;
 import ch.fhnw.oop2.hydropowerfx.view.ViewMixin;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 
 public class Editor extends HBox implements ViewMixin {
@@ -11,10 +13,15 @@ public class Editor extends HBox implements ViewMixin {
     private RootPM rootPM;
     private RootPanel rootPanel;
     private Label editorStationName;
+    private TextField maxPower;
+    private Label canton;
+
+    private PowerStation actualStation;
 
     public Editor(RootPM rootPM, RootPanel rootPanel) {
         this.rootPM = rootPM;
         this.rootPanel = rootPanel;
+        this.actualStation = rootPM.getActualPowerStation();
         this.init();
     }
 
@@ -28,21 +35,35 @@ public class Editor extends HBox implements ViewMixin {
     public void initializeControls() {
         editorStationName = new Label();
         editorStationName.getStyleClass().addAll("editor-stationname", "title");
+
+        maxPower = new TextField();
+        canton = new Label();
     }
 
     @Override
     public void layoutControls() {
-        getChildren().addAll(editorStationName);
+        getChildren().addAll(editorStationName, maxPower, canton);
     }
 
     @Override
     public void setupBindings() {
         editorStationName.textProperty().bind(rootPM.getActualPowerStation().nameProperty());
+        maxPower.textProperty().bindBidirectional(rootPM.getActualPowerStation().maxPowerProperty());
+        canton.textProperty().bind(rootPM.getActualPowerStation().cantonProperty());
+    }
+
+    public void releaseBindings() {
+        editorStationName.textProperty().unbind();
+        canton.textProperty().unbind();
+        maxPower.textProperty().unbindBidirectional(actualStation.maxPowerProperty());
     }
 
     @Override
     public void setupEventHandlers() {
         rootPM.actualPowerStationProperty().addListener(event -> {
+            releaseBindings();
+            actualStation = rootPM.getActualPowerStation();
+            maxPower.setText(rootPM.getActualPowerStation().getMaxPower());
             setupBindings();
         });
     }
