0a8806e7d538a460f70663ee406a3d2a22450a40
rogerkreienbuehl <r.kreienbuehl>
2018-05-22 20:10:33 +0200

    added two buttons

--- src/main/java/ch/fhnw/oop2/hydropowerfx/view/preferences/PreferencesPanel.java
+++ src/main/java/ch/fhnw/oop2/hydropowerfx/view/preferences/PreferencesPanel.java
@@ -2,6 +2,7 @@ package ch.fhnw.oop2.hydropowerfx.view.preferences;
 
 import ch.fhnw.oop2.hydropowerfx.presentationmodel.RootPM;
 import ch.fhnw.oop2.hydropowerfx.view.ViewMixin;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
 import java.util.List;
@@ -9,6 +10,9 @@ import java.util.List;
 public class PreferencesPanel extends VBox implements ViewMixin {
 
     private RootPM rootPM;
+
+    private Button cancel;
+    private Button save;
 
     public PreferencesPanel(RootPM rootPM) {
         this.rootPM = rootPM;
@@ -22,10 +26,18 @@ public class PreferencesPanel extends VBox implements ViewMixin {
     }
 
     @Override public void initializeControls() {
-
+        cancel = new Button("Abbrechen");
+        save = new Button("Speichern");
     }
 
     @Override public void layoutControls() {
+        getChildren().addAll(cancel, save);
+    }
 
+    @Override
+    public void setupBindings() {
+        cancel.setOnAction(event -> {
+            cancel.getScene().getWindow().hide();
+        });
     }
 }
