a919208925ce186940a7c2155c6795381784d9d1
Florian Thievent <f.jeremias@me.com>
2018-05-28 23:05:02 +0200

    added style classes to preference panel

--- src/main/java/ch/fhnw/oop2/hydropowerfx/view/preferences/PreferencesPanel.java
+++ src/main/java/ch/fhnw/oop2/hydropowerfx/view/preferences/PreferencesPanel.java
@@ -2,13 +2,8 @@ package ch.fhnw.oop2.hydropowerfx.view.preferences;
 
 import ch.fhnw.oop2.hydropowerfx.presentationmodel.RootPM;
 import ch.fhnw.oop2.hydropowerfx.view.ViewMixin;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class PreferencesPanel extends VBox implements ViewMixin {
@@ -27,18 +22,23 @@ public class PreferencesPanel extends VBox implements ViewMixin {
     private ToggleButton sqliteButton;
     private ToggleButton neo4jButton;
 
+    private HBox buttonBox;
+
     public PreferencesPanel(RootPM rootPM) {
         this.rootPM = rootPM;
-
         init();
     }
 
     @Override
     public void initializeSelf() {
+        addStylesheetFiles("../assets/style.css");
         this.getStyleClass().add("preferences-panel");
     }
 
-    @Override public void initializeControls() {
+    @Override
+    public void initializeControls() {
+        getStyleClass().add("preference-panel");
+
         tabPane = new TabPane();
         tabPane.setPrefSize(400, 360);
         tabPane.setMinSize(TabPane.USE_PREF_SIZE, TabPane.USE_PREF_SIZE);
@@ -54,24 +54,33 @@ public class PreferencesPanel extends VBox implements ViewMixin {
         dbTab.setContent(vbox);
 
         dbLabel = new Label();
+        dbLabel.getStyleClass().add("preference-title");
 
         dbGroup = new ToggleGroup();
         csvButton = new RadioButton();
         csvButton.setToggleGroup(dbGroup);
+        csvButton.getStyleClass().add("preference-radio");
         sqliteButton = new RadioButton();
         sqliteButton.setToggleGroup(dbGroup);
+        sqliteButton.getStyleClass().add("preference-radio");
         neo4jButton = new RadioButton();
         neo4jButton.setToggleGroup(dbGroup);
+        neo4jButton.getStyleClass().add("preference-radio");
         vbox.getChildren().addAll(dbLabel, csvButton, sqliteButton, neo4jButton);
 
         tabPane.getTabs().add(dbTab);
-
+        buttonBox = new HBox();
         cancel = new Button("Abbrechen");
+        cancel.getStyleClass().add("button-primary");
         save = new Button("Speichern");
+        save.getStyleClass().add("button-light");
+
     }
 
-    @Override public void layoutControls() {
-        getChildren().addAll(tabPane, cancel, save);
+    @Override
+    public void layoutControls() {
+        buttonBox.getChildren().addAll(cancel, save);
+        getChildren().addAll(tabPane, buttonBox);
     }
 
     @Override
