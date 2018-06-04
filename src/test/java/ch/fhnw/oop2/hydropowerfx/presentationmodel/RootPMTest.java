73275a84bcab42a1ebe6f36f095e861155bbe4cc
Florian Thievent <f.jeremias@me.com>
2018-06-04 15:44:33 +0200

    added Test for canton initializiation

--- src/test/java/ch/fhnw/oop2/hydropowerfx/presentationmodel/RootPMTest.java
+++ src/test/java/ch/fhnw/oop2/hydropowerfx/presentationmodel/RootPMTest.java
@@ -1,18 +1,25 @@
 package ch.fhnw.oop2.hydropowerfx.presentationmodel;
 
+import ch.fhnw.oop2.hydropowerfx.view.footer.CantonList;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RootPMTest {
 
     @Test
-    void testSetup(){
+    void testGetCantons(){
         //given
+        RootPM rootpm = new RootPM();
 
         //when
+        List<Canton> canton = rootpm.getCantons();
 
         //then
-        assertTrue(0 == 0);
+        Assertions.assertEquals(26, canton.size());
     }
 }
