7674f938fc1cab96b9f6c14101de773e51599308
rogerkreienbuehl <r.kreienbuehl>
2018-06-05 20:02:01 +0200

    tried to solve font problem

--- src/main/java/ch/fhnw/oop2/hydropowerfx/HydroPowerApp.java
+++ src/main/java/ch/fhnw/oop2/hydropowerfx/HydroPowerApp.java
@@ -40,7 +40,9 @@ public class HydroPowerApp extends Application {
 
         Font.loadFont(HydroPowerApp.class.getResource("view/assets/fonts/Montserrat-Bold.ttf").toExternalForm(), 10);
         Font.loadFont(HydroPowerApp.class.getResource("view/assets/fonts/Rubik-Regular.ttf").toExternalForm(), 10);
-        Font.loadFont(HydroPowerApp.class.getResource("view/assets/fonts/HandofSean.ttf").toExternalForm(), 10);
+        // Font.loadFont(HydroPowerApp.class.getResource("view/assets/fonts/HandofSean.ttf").toExternalForm(), 10);
+        Font.loadFont( HydroPowerApp.class.getResourceAsStream( "/ch/fhnw/oop2/hydropowerfx/view/assets/fonts/HandofSean.ttf"), 10);
+        // System.out.println(font);
 
         splashStage = new Stage();
         showSplash(splashStage);
